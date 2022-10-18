6711d776922e93b1f6f8d4f6ffcb3d7b52e33e8b
rudex339 <58317478+rudex339@users.noreply.github.com>
2022-10-19 01:23:04 +0900

    1-12 1-13

--- vertex.glsl
+++ vertex.glsl
@@ -1,11 +1,13 @@
-#version 330
-//--- in_Position: attribute index 0 
-//--- in_Color: attribute index 1
-layout (location = 0) in vec3 in_Position; //--- 위치 변수: attribute position 0
-layout (location = 1) in vec3 in_Color; //--- 컬러 변수: attribute position 1
+#version 330 core
+
+layout (location = 0) in vec3 aPos;   // 위치 변수는 attribute position 0을 가집니다.
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec3 in_Color; //--- 컬러 변수: attribute position 1
 out vec3 out_Color; //--- 프래그먼트 세이더에게 전달
-void main(void) 
+
+uniform mat4 modelTransform;
+void main()
 {
-	gl_Position = vec4 (in_Position.x, in_Position.y, in_Position.z, 1.0);
+	gl_Position = modelTransform * vec4(aPos, 1.0); 
 	out_Color = in_Color;
-}
+}  
